a939e510fc5ed27d5194efaba8bfc8d9355a4cba
ps <psncl@ps.ncl>
2024-05-02 01:09:25 +0100

    Start manage user page

---
+++ src/routes/app/users/manage/+page.js
@@ -0,0 +1,23 @@
+// import { items } from '../data.js';
+import { error } from '@sveltejs/kit';
+
+export function load() {
+	const users = [
+		{
+			name: 'Marcus Chan',
+			userid: 'MC0929',
+			email: 'mchan@dyson.com',
+			role: 'Manager'
+		},
+		{
+			name: 'M Chan',
+			userid: 'MC0121',
+			email: 'mc@dyson.com',
+			role: 'Senior Logistics'
+		}
+	];
+
+	if (!users) throw error(404);
+
+	return { users };
+}
---
+++ src/routes/app/users/manage/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+	import * as Table from '$lib/components/ui/table';
+	export let data;
+</script>
+
+<div class="mycontainer">
+	<header>
+		<h2>User Management</h2>
+	</header>
+
+	<Table.Root>
+		<Table.Header>
+			<Table.Row class={'bg-green-600 hover:bg-green-600'}>
+				<Table.Head class={'w-[200px] text-gray-50'}>Name</Table.Head>
+				<Table.Head class={'text-gray-50'}>User ID</Table.Head>
+				<Table.Head class={'text-gray-50'}>Email</Table.Head>
+				<Table.Head class={'text-gray-50'}>Role</Table.Head>
+				<Table.Head class={'text-center text-gray-50'}>Edit</Table.Head>
+			</Table.Row>
+		</Table.Header>
+		<Table.Body>
+			{#each data.users as user, i (i)}
+				<Table.Row>
+					<Table.Cell class="font-medium">{user.name}</Table.Cell>
+					<Table.Cell>{user.userid}</Table.Cell>
+					<Table.Cell>{user.email}</Table.Cell>
+					<Table.Cell>{user.role}</Table.Cell>
+					<Table.Cell class="text-center">Edit</Table.Cell>
+				</Table.Row>
+			{/each}
+		</Table.Body>
+	</Table.Root>
+</div>
+
+<style>
+	@import '$lib/app.css';
+
+	.mycontainer {
+		width: 70%;
+		margin: 5rem auto 0 auto;
+		border: 1px solid #000;
+		border-radius: 0.5rem;
+	}
+
+	header {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		background-color: var(--bg-accent-color);
+		color: white;
+		text-align: center;
+		padding: 1rem 0;
+		grid-column: 1 / -1;
+		border-radius: 0.5rem;
+	}
+
+	header h2 {
+		font-size: 2rem;
+		font-weight: 500;
+	}
+</style>
